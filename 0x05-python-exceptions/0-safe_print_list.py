29f16fd1b12e8b3e9a01816786c4af5400472b5f
gentomacine <ogwaluchidozie@gmail.com>
2022-06-20 17:25:39 +0100

    commited

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,10 @@ def safe_print_list(my_list=[], x=0):
     for idx in range(x):
         try:
             print(f'{my_list[idx]}', end='')
-            
+
         except (IndexError):
             continue
+        
         counter += 1
     print()
     return (counter)
