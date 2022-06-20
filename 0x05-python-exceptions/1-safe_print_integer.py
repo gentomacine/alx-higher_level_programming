f5669fa16231ac83538303f5b29b3e0e77a162b8
gentomacine <ogwaluchidozie@gmail.com>
2022-06-20 16:35:54 +0100

    commited

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-    except:
+        print('{:d}'.format(value))
+    except (Exception):
         return (False)
-    else:
-        return (True)
+    return True
+    
